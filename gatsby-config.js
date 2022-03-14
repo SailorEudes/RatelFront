3513e77951eda984b207c855a33c6a63519d8446
Eudes <36993464+SailorEudes@users.noreply.github.com>
2022-03-14 14:16:21 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -82,7 +82,7 @@ module.exports = {
         {
             resolve: "gatsby-source-strapi",
             options: {
-              apiURL: "https://rateladmin.herokuapp.com/",
+              apiURL: "https://rateladmin.herokuapp.com/api",
               collectionTypes: [
                 "avis",
                 "les-pluses",
