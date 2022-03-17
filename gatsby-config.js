dd00f96f0fa772b8e07bf1bcf81fbb91e6d14911
Eudes <36993464+SailorEudes@users.noreply.github.com>
2022-03-17 14:12:39 +0100

    Update

--- gatsby-config.js
+++ gatsby-config.js
@@ -82,7 +82,7 @@ module.exports = {
         {
             resolve: `gatsby-source-strapi`,
             options: {
-                apiURL: `http://localhost:1337`,
+                apiURL: `https://rateladmin.herokuapp.com/api`,
                 collectionTypes: [
                     {
                         name: `stat`,
